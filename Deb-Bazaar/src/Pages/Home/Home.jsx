e798bded8ea2676ff0743efe0629efb195e8c602
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-01-21 23:21:06 +0530

    add some compoents

--- Deb-Bazaar/src/Pages/Home/Home.jsx
+++ Deb-Bazaar/src/Pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import Hero from '../../Component/Home/Hero'
-import Sale from '../../Component/Home/Sale'
+import Hero from '../../Component/Home/Hero Section'
+import Sale from '../../Component/Home/Sale Section'
+import Categories from '../../Component/Home/Browse Categories section'
+import BestSelling from '../../Component/Home/BestSelling'
+import Categories_section from '../../Component/Home/Categories_section'
+import Product from '../../Component/Home/Product Section'
+import New_Arrive from '../../Component/Home/New_Arrive'
 
 const Home = () => {
   return (
-    <div className='flex flex-col gap-5 w-[80vw] m-auto'>
+    <div className='flex flex-col gap-7 m-auto w-[90%]'>
       <Hero/>
       <Sale/>
+      <hr className='w-[90%] m-auto' />
+      <Categories/>
+      <hr className='w-[90%] m-auto' />
+      <BestSelling/>
+      <Categories_section/>
+      <Product/>
+      <New_Arrive/>
     </div>
   )
 }
