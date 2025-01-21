2867b82724aa4c7bc01189f2dc9c9b25c3ee53a1
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-01-21 23:20:22 +0530

    add images url

--- Deb-Bazaar/tailwind.config.js
+++ Deb-Bazaar/tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
+  	extend: {backgroundImage: {
+      'PS5': "url('/public/Ps5.png')",
+      'WomenCollection' : "url('/public/Women collection.png')" ,
+      'Speaker': "url('/public/Speaker.png')",
+      'Perfume': "url('/public/Perfume.png')",
+    },
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
