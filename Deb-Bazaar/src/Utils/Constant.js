65d0e2e90babf99dbeb403fabdec755d7ba66e75
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-01-21 23:20:39 +0530

    add some api url

--- Deb-Bazaar/src/Utils/Constant.js
+++ Deb-Bazaar/src/Utils/Constant.js
@@ -1,8 +1,16 @@
 export const  HOST="http://localhost:3000";
 
-export const AUTH_ROUTES='/api/auth';
-export const GET_PRODUCT_DATA='/Product/getProduct'
+export const GET_PRODUCT_DATA='/Product/getProduct';
 
+export const AUTH_ROUTES='/api/auth';
 export const SIGNUP_ROUTES=`${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTES=`${AUTH_ROUTES}/login`;
+
+export const CART='cart';
+export const ADD_TO_CART=`${CART}/addToCart`;
+
+export const WISHLIST='wishList'
+export const ADD_TO_WISHLIST=`${WISHLIST}/addToWishList`;
+
+
 
