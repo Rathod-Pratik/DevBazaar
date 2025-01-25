3a59ac0c3a4563f114e3374f358bfb4fd7458678
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-01-25 23:14:07 +0530

    add some url

--- Deb-Bazaar/src/Utils/Constant.js
+++ Deb-Bazaar/src/Utils/Constant.js
@@ -8,9 +8,15 @@ export const LOGIN_ROUTES=`${AUTH_ROUTES}/login`;
 
 export const CART='cart';
 export const ADD_TO_CART=`${CART}/addToCart`;
+export const GET_CART=`${CART}/getCart`
+export const DELETE_FROM_CART=`${CART}/deleteFromCart`
 
 export const WISHLIST='wishList'
 export const ADD_TO_WISHLIST=`${WISHLIST}/addToWishList`;
+export const REMOVE_FROM_WISHLIST=`${WISHLIST}/deleteFromWishList`;
+export const GET_WISHLIST=`${WISHLIST}/getWishList`;
+
+export const BILLING='Billing/getBilling';
 
 
 
