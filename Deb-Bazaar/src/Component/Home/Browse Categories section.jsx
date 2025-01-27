44835001169e1055ed40d192bcd09ead12128c9a
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-01-27 23:35:13 +0530

    Update Browse Categories section.jsx

--- Deb-Bazaar/src/Component/Home/Browse Categories section.jsx
+++ Deb-Bazaar/src/Component/Home/Browse Categories section.jsx
@@ -1,29 +1,36 @@
 import React from "react";
+import { PiHeadphonesThin } from "react-icons/pi";
+import { FiCamera } from "react-icons/fi";
+import { TbDeviceGamepad } from "react-icons/tb";
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { LuWatch } from "react-icons/lu";
+import { FiSmartphone } from "react-icons/fi";
+
 const Categories = () => {
-  const Categories = [
+  const categories = [
     {
       name: "Phone",
-      url: "/Category-CellPhone.svg",
+      icon: <FiSmartphone />,
     },
     {
       name: "Computer",
-      url: "/Category-Computer.svg",
+      icon: <HiOutlineComputerDesktop />,
     },
     {
       name: "Watch",
-      url: "/Category-SmartWatch.svg",
+      icon: <LuWatch />,
     },
     {
       name: "Camera",
-      url: "/Category-Camera.svg",
+      icon: <FiCamera />,
     },
     {
       name: "HeadPhone",
-      url: "/Category-Headphone.svg",
+      icon: <PiHeadphonesThin />,
     },
     {
       name: "Gaming",
-      url:'/Category-Gamepad.svg'
+      icon: <TbDeviceGamepad />,
     },
   ];
 
@@ -31,15 +38,20 @@ const Categories = () => {
     <div>
       <div className="flex flex-col gap-5 mx-3 my-3">
         <div className="flex flex-row gap-3 text-lg font-semibold text-red-600">
-          <span className="bg-red-600  px-2 py-1 rounded">A</span>
+          <span className="bg-red-600 px-2 py-1 rounded">A</span>
           Categories
         </div>
         <div className="text-3xl font-medium">Browse By Category</div>
       </div>
       <div className="flex flex-row justify-center flex-wrap gap-8">
-        {Categories.map((data, index) => (
-          <div key={index} className="border-black hover:bg-[#DB4444] hover:text-white hover:border-none p-4 border-solid border-[1px] mx-3 my-2 w-[170px] h-[145px] gap-3 flex flex-col items-center transition-all duration-300">
-            <img src={data.url} className="hover:text-white"  alt={data.name} />
+        {categories.map((data, index) => (
+          <div
+            key={index}
+            className="border-black hover:bg-[#DB4444] hover:text-white hover:border-none p-4 border-solid border-[1px] mx-3 my-2 w-[170px] h-[145px] gap-3 flex flex-col items-center justify-center transition-all duration-300"
+          >
+            <div className="text-4xl  hover:text-white">
+              {data.icon}
+            </div>
             <p>{data.name}</p>
           </div>
         ))}
