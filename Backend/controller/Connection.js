c4a02eafe8d1758236d966392c81a1ecc09ad323
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-22 11:43:10 +0530

    Update Connection.js

--- Backend/controller/Connection.js
+++ Backend/controller/Connection.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
  export function connectToMongo(url) {
-    return mongoose.connect(url, { // Return the promise from mongoose.connect
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    return mongoose.connect(url);
 }
 
 
